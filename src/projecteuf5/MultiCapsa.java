897be65f7a673bfa9cc096e8f97e0c5ad02d8a42
manel <manelgavalda@iesebre.com>
2016-11-07 17:55:37 +0100

    ex2 corregit

--- src/projecteuf5/MultiCapsa.java
+++ src/projecteuf5/MultiCapsa.java
@@ -5,9 +5,11 @@
  */
 package projecteuf5;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  *
@@ -25,38 +27,48 @@ contingut=implementacio;
 
 //Implementació dels altres mètodes
 public boolean esBuida(){
-    if(contingut.isEmpty()){
-        return true;
-    }
-    return false;
-    
+    return this.contingut.isEmpty();
 }
 
 public void afegir(E e){
-
     contingut.add(e);
-    
 }
 
-public Object obtenir(){
+public E obtenir(){
     
     if(contingut.isEmpty()){
-        return false;
+        return null;
     }
     return contingut.iterator().next();
 }
 
-public void eliminar(){
-    contingut.remove("Primer");
+public boolean eliminar(){
+    E e=this.obtenir();
+    if(!e.equals(null))
+        return this.contingut.remove(e);
+    else
+        return false;
 }
 
     public boolean buidar() {
-        if(contingut.isEmpty()){
+        if(!contingut.isEmpty()){
             return false;
         }
         contingut.clear();
         return true;
     }
 
+    /*
+    3. Afegiu a la MultiCapsa un mètode genèric que retorne el seu contingut en ordre creixent, 
+    fent els canvis necessaris per a que ho puguem fer en qualsevol classe sense possibilitat 
+    de rebre errors en temps d'execució per causa del tipus utilitzat. 
+    Implementar la capsa intentant posar bicicletes (classe Bicycle). 
+    Heu pogut? Modifiqueu les bicicletes fins aconseguir-ho. 
+    La capçalera (no del tot correcta) del mètode serà pareguda a la següent:
+    */
+    public static <E extends Comparable> E ordenar(Collection <E> contingut){
+        List <E> list = new ArrayList<>(contingut);
+        return Collections.sort(list);
+}
 }
 
