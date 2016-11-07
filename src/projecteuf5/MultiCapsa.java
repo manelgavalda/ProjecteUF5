a49e582ca2225edd58221da49c7d4a4affed9302
manel <manelgavalda@iesebre.com>
2016-11-07 18:11:51 +0100

    Ex3

--- src/projecteuf5/MultiCapsa.java
+++ src/projecteuf5/MultiCapsa.java
@@ -70,6 +70,43 @@ public boolean eliminar(){
         List list = new ArrayList<>(this.contingut);
         Collections.sort(list);
         return list;
+        
+        
+}
+    public class Bicycle implements Comparable<Bicycle>{
+        
+            @Override
+        public int compareTo(Bicycle o) {
+            return this.cadence-o.cadence;
+        }
+    
+    int cadence = 0;
+    int speed = 0;
+    int gear = 1;
+
+    void changeCadence(int newValue) {
+         cadence = newValue;
+    }
+
+    void changeGear(int newValue) {
+         gear = newValue;
+    }
+
+    void speedUp(int increment) {
+         speed = speed + increment;   
+    }
+
+    void applyBrakes(int decrement) {
+         speed = speed - decrement;
+    }
+
+    void printStates() {
+         System.out.println("cadence:" +
+             cadence + " speed:" + 
+             speed + " gear:" + gear);
+    }
+
+
 }
 }
 
