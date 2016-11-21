d923b840f10d1285089ce0d4eeaa416264e2a21c
manel <manelgavalda@iesebre.com>
2016-11-21 17:15:11 +0100

    Classe Jugador

--- src/model/Jugador.java
+++ src/model/Jugador.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package model;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,22 +16,16 @@ import java.util.Set;
 public class Jugador {
 
     //HashSet per tindre una llista dels jugadors perquè no és repeteixin.
-    private final Set <Equip>haJugat = new <Equip>HashSet();    
-    private String  nom;
-    private int numero;
-    private int altura;
-    private int pes;
-    private int dataNaixement;
-    private String jugaPer;
+    private String  _1nom;
+    private String  _2email;
+    private int _3numero;
+    private float _4altura;
+    private int _5pes;
+    private int _6dataNaixement;
+    private String _7jugaPer;
+    private ArrayList _8haJugatPer; //Ficarem en ArrayList ja que tindrem una llista per cada jugador(1) a tots els equips que ha jugat(0,n).
     
-        //Ficarem en ArrayList ja que tindrem una llista per cada jugador(1) a tots els equips que ha jugat(0,n).
-    public Jugador(String nom, int numero, int altura, int pes, int dataNaixement, String jugaPer) {
-        this.nom = nom;
-        this.numero = numero;
-        this.altura = altura;
-        this.pes = pes;
-        this.dataNaixement = dataNaixement;
-        this.jugaPer = jugaPer;
-    }
+        
+
     
 }
